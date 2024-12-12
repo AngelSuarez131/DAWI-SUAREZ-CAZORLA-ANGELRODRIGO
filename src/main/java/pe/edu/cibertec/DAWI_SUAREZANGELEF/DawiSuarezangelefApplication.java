2ccb0fa9ae0e628c9073bdafaa44b69a9ad34575
Angel <angelsuarez131@gmail.com>
2024-12-12 12:14:52 -0500

    Primer commit EF

---
+++ src/main/java/pe/edu/cibertec/DAWI_SUAREZANGELEF/DawiSuarezangelefApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_SUAREZANGELEF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiSuarezangelefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiSuarezangelefApplication.class, args);
+	}
+
+}
